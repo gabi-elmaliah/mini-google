fc88f6f2486fdcd0b4f36e6090adcbbc3fc7536c
Gabby Elmaliah <gabbyelmaliah@gmail.com>
2026-04-02 11:28:32 +0100

    initial commit

---
+++ src/test/java/com/gabbyelmaliah/minigoogle/MiniGoogleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabbyelmaliah.minigoogle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniGoogleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

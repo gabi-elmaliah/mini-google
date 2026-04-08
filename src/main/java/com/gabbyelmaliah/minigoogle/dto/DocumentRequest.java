17d960a8e94ee1d585f7f55ada8df5264cedd527
Gabby Elmaliah <gabbyelmaliah@gmail.com>
2026-04-08 04:16:15 +0100

    fixed kafka issues

--- src/main/java/com/gabbyelmaliah/minigoogle/dto/DocumentRequest.java
+++ src/main/java/com/gabbyelmaliah/minigoogle/dto/DocumentRequest.java
@@ -8,7 +8,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class DocumentRequest {
-    private String id;
     private String title;
     private String content;
 }
--- src/main/java/com/gabbyelmaliah/minigoogle/mapper/DocumentMapper.java
+++ src/main/java/com/gabbyelmaliah/minigoogle/mapper/DocumentMapper.java
@@ -4,11 +4,13 @@ import com.gabbyelmaliah.minigoogle.dto.DocumentRequest;
 import com.gabbyelmaliah.minigoogle.dto.DocumentResponse;
 import com.gabbyelmaliah.minigoogle.model.DocumentEntity;
 
+import java.util.UUID;
+
 public class DocumentMapper {
 
     public static DocumentEntity toEntity(DocumentRequest request) {
         return DocumentEntity.builder()
-                .id(request.getId())
+                .id(UUID.randomUUID().toString())
                 .title(request.getTitle())
                 .content(request.getContent())
                 .build();
